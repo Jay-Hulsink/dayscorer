6171828215a6eaf992ac98add1db0991703cce29
Jay Hulsink <574898@edu.rocmn.nl>
2025-03-10 15:39:22 +0100

    made changes to login page, no function added yet

--- login.php
+++ login.php
@@ -9,10 +9,12 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>Please Login</h1>
+        <div class="top_bar"><h1>Dayscorer</h1></div>
+        <h2>Please Login</h2>
             <form method="post">
                 <input type="text" name="login_username" placeholder="username">
                 <input type="password" name="login_pass" placeholder="password">
+                <input class="submit_button" type="submit" name="log" value="log in">
             </form>
 
         <h2>Don't have an account? Sign up here</h2>
@@ -20,6 +22,7 @@
                 <input type="text" name="username" placeholder="username">
                 <input type="password" name="pass_first" placeholder="password">
                 <input type="password" name="pass_repeat" placeholder="repeat password">
+                <input class="submit_button" type="submit" name="sign" value="sign up">
             </form>
     </body>
 </html>
